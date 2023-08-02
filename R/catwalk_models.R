9d40f317b659d28bd7244dbf0aac7dd06cb98d8b
SkulGurl <Creasmandana@gmail.com>
2023-08-02 13:23:05 -0700

    UMAP functionality implemented
    issues with "catwalk_umap()" function fixed, now properly allows user to perform umap analysis and visualiziation of CatwalkDataset objects.

--- R/catwalk_models.R
+++ R/catwalk_models.R
@@ -8,7 +8,7 @@
 #' @param cutoff The value at which p-values lower than this number will be considered "statistically significant"
 #' @param make_default A boolean that will designate the output of this analysis as the default output to be reference for any future visualization or follow-up analyses
 #' @param allow_duplicates A boolean that designates whether or not to allow the creation of two sets of models using the same data, formula, and cutoff within the same CatwalkDataset
-#'
+#' @return A CatwalkDataset object containing the newly generated models
 #' @export
 catwalk_anovas <-
   function(catwalkdataset,
@@ -21,8 +21,8 @@ catwalk_anovas <-
     data <-
       catwalk_get_data_from_dataset(catwalkdataset, dataname = dataname)
 
-    for (name in dataname){
-      if(name %in% names(catwalkdataset@data)){
+    for (name in dataname) {
+      if (name %in% names(catwalkdataset@data)) {
         data_used <- name
         break
       }
@@ -37,15 +37,17 @@ catwalk_anovas <-
 
     for (model in catwalkdataset@models) {
       if (!allow_duplicates &&
-        data_used == model$data_used &&
+          data_used == model$data_used &&
           formula == model$formula_used &&
-          cutoff == model$cutoff){
-        warning("Warning: Models using this combination of formula, cuttoff, and dataset exist already within this CatwalkDataset, execution ceased before duplicate models were generated. Set the argument 'allow_duplicates' = TRUE to bypass this.")
+          cutoff == model$cutoff) {
+        warning(
+          "Warning: Models using this combination of formula, cuttoff, and dataset exist already within this CatwalkDataset, execution ceased before duplicate models were generated. Set the argument 'allow_duplicates' = TRUE to bypass this."
+        )
         return(catwalkdataset)
       }
 
 
-      }
+    }
 
     variable_names <- data %>%
       dplyr::select(!c("group", "time_point", "animal", "experiment")) %>%
@@ -57,7 +59,7 @@ catwalk_anovas <-
       aov_models <- c(aov_models, list(aovmodel))
     }
     aov_summaries <-
-      aov_models %>% purrr::map(~ summary(.x)[[1]]) %>% purrr::map(~ fix_names(.x))
+      aov_models %>% purrr::map( ~ summary(.x)[[1]]) %>% purrr::map( ~ fix_names(.x))
     names(aov_summaries) <- variable_names
     names(aov_models) <- variable_names
 
@@ -86,7 +88,8 @@ catwalk_anovas <-
     for (parameter_name in names(aov_summaries)) {
       temp <- all_pvals %>% dplyr::filter(parameter == parameter_name)
       if (!any(temp$pval < cutoff)) {
-        sig_pvals <- sig_pvals %>% dplyr::filter(parameter != parameter_name)
+        sig_pvals <-
+          sig_pvals %>% dplyr::filter(parameter != parameter_name)
       }
     }
 
@@ -138,9 +141,11 @@ fix_names <- function(data) {
 sigmoidal_trans <- function(a = 40, cutoff = 0.05) {
   scales::trans_new("sigmoidal",
                     # The forward transformation maps x-values to y-values on the sigmoidal scale
-                    function(x) 1 / (1 + exp(1 * a * (x - cutoff))),
+                    function(x)
+                      1 / (1 + exp(1 * a * (x - cutoff))),
                     # The reverse transformation maps y-values back to x-values on the original scale
-                    function(x) -1 / a * (log(x) - log(1 - x)) + cutoff)
+                    function(x)
+                      - 1 / a * (log(x) - log(1 - x)) + cutoff)
 }
 
 #' Create a heatmap of significant p-values from an ANOVA model
@@ -155,7 +160,6 @@ sigmoidal_trans <- function(a = 40, cutoff = 0.05) {
 
 catwalk_heatmap_aov <-
   function(catwalkdataset, anova_results = 1) {
-
     # extract data from the ANOVA results of the given index
     data_anova_results <- catwalkdataset@models[[anova_results]]
 
@@ -209,15 +213,19 @@ catwalk_venn <- function(catwalkdataset,
 
   # issue warning if more than 4 factors to be plotted
   if (length(hits) > 4) {
-    warning("Results have more than 4 factors/combinations of factors, will only graph first 4. Use the 'subset' argument to manually decide which factors to include in graph")
+    warning(
+      "Results have more than 4 factors/combinations of factors, will only graph first 4. Use the 'subset' argument to manually decide which factors to include in graph"
+    )
   }
 
   # create Venn diagram using ggvenn
   venn <- ggvenn(hits) +
-    theme(panel.background = element_rect(fill = "white",
-                                          colour = "white",
-                                          size = 0.5,
-                                          linetype = "solid"))
+    theme(panel.background = element_rect(
+      fill = "white",
+      colour = "white",
+      size = 0.5,
+      linetype = "solid"
+    ))
   venn
 }
 
@@ -231,22 +239,26 @@ catwalk_venn <- function(catwalkdataset,
 #' @param umap_config A list of configuration parameters for the UMAP algorithm, see `umap.defaults` in the umap package for a full list of available parameters.
 #' @param return A string indicating the output format. Valid options are "plot" (default), "data", or "all".
 #' @param color A string indicating the name of the column in the data set to be used for color-coding points in the plot.
+#' default is for all points to be colored black.
 #'
 #' @return If \code{return} is "plot" (default), returns a ggplot object showing the UMAP projection of the data.
 #' If \code{return} is "data", returns a data frame with the UMAP coordinates and the columns from the input data set.
 #' If \code{return} is "all", returns a list with the UMAP configuration parameters, the input data set, and the ggplot object.
+#' @import umap
+#' @import ggplot2
 #' @export
 catwalk_umap <- function(catwalkdataset,
                          dataname = c("data_norm", "data_clean"),
                          umap_config = umap.defaults,
                          return = "plot",
-                         color = NULL) {
-
+                         color = "group") {
   # get the data set
-  data <- catwalk_get_data_from_dataset(catwalkdataset, dataname = dataname)
+  data <-
+    catwalk_get_data_from_dataset(catwalkdataset, dataname = dataname)
 
   # perform the UMAP embedding
-  umap <- umap(data %>% select(!c(animal, time_point, experiment)) %>% select_if(is.numeric), config = umap_config)
+  umap <-
+    umap(data %>% select(!c(animal, time_point, experiment)) %>% select_if(is.numeric), config = umap_config)
 
   # combine the UMAP coordinates with the input data set
   data_umap <- as.data.frame(umap$layout) %>% cbind(data)
@@ -256,17 +268,36 @@ catwalk_umap <- function(catwalkdataset,
     return(data_umap)
   } else {
     # plot the UMAP projection
-    plot <- ggplot(data_umap, aes(x = V2, y = V1, color = data_umap %>% dplyr::pull(color))) +
+
+    #look for color variable
+    if (!is.null(color)){
+      color <- data_umap %>% dplyr::pull(color)
+    }
+
+
+    plot <-
+      ggplot(data_umap,
+             aes(
+               x = V2,
+               y = V1,
+               color = color
+             )) +
       geom_point() + theme_A86() +
-      theme(axis.title = element_blank(),
-            axis.text = element_blank(),
-            axis.ticks = element_blank(),
-            legend.title = element_blank())
+      theme(
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.title = element_blank()
+      )
 
     if (return == "plot") {
       return(plot)
     } else if (return == "all") {
-      return(list(config = umap_config, data = data, plot = plot))
+      return(list(
+        config = umap_config,
+        data = data,
+        plot = plot
+      ))
     } else {
       stop("invalid 'return' argument provided. Valid inputs are 'data', 'plot', or 'all'")
     }
@@ -301,17 +332,22 @@ catwalk_tag_analysis <-
 
 
     hits <- data_anova_results$hits
-    if(!is.null(factors)){
-      missing_factors <- factors[!(factors%in%names(hits))]
-      if(length(missing_factors)>0){
-        if(length(missing_factors) == length(factors)){
+    if (!is.null(factors)) {
+      missing_factors <- factors[!(factors %in% names(hits))]
+      if (length(missing_factors) > 0) {
+        if (length(missing_factors) == length(factors)) {
           stop("no factors found with the factor names provided")
         }
         else{
-          warning(paste0("the following factor could not be found and was not included in the analysis: ", missing_factors))
+          warning(
+            paste0(
+              "the following factor could not be found and was not included in the analysis: ",
+              missing_factors
+            )
+          )
         }
       }
-      hits <- hits[names(hits)%in%factors]
+      hits <- hits[names(hits) %in% factors]
     }
     tags_sig =  data.frame(tag = unique(unlist(hits)) %>%
                              catwalk_variables_to_tags(split_by = split_by)) %>%
